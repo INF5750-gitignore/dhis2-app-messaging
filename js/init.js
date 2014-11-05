e40a4f831aef3bb63a5d838c0cf5b4670818c94b
Jean-Charles Leneveu <leneveu.jc@gmail.com>
2014-11-05 11:26:50 +0000

    Added a init.js file, which initialize dhisapi variable from the manifest.webapp

---
+++ js/init.js
@@ -0,0 +1,9 @@
+/* This initialization code gets the web api url from the manifest file
+* and makes it available as a Javascript variable (for Angular).
+* This code is taken from the example application of Lars Kristian Roland*/
+
+ var xhReq = new XMLHttpRequest();
+xhReq.open("GET", "manifest.webapp", false);
+xhReq.send(null);
+var serverResponse = JSON.parse(xhReq.responseText);
+var dhisAPI = serverResponse.activities.dhis.href;
