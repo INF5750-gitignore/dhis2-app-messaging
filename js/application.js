827e80e96f7fd80b72e0c7f378413684b1ea805b
Jean-Charles Leneveu <leneveu.jc@gmail.com>
2014-11-10 21:03:56 +0000

    Added infinite scrolling

--- js/application.js
+++ js/application.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var app = angular.module('app', ['ngRoute']);
+var app = angular.module('app', ['ngRoute', 'infinite-scroll']);
 
 function make_base_auth(user, password) {
     var tok = user + ':' + password;
@@ -8,19 +8,39 @@ function make_base_auth(user, password) {
     return 'Basic ' + hash;
 }
 
-app.controller('ShowAllMessages', function ($scope) {
-    $.ajax({
-        url: "http://inf5750-8.uio.no/api/messageConversations.json?paging=false&fields=id,created,displayName,followUp,lastSenderSurname,lastSenderFirstname,read,messageCount",
-        type: 'GET',
-        dataType: 'json',
-        async: false,
-        success: function (data) {
-            $scope.messages = data.messageConversations;
-        },
-        beforeSend: function (xhr) {
-            xhr.setRequestHeader('Authorization', make_base_auth("admin", "district"));
-        }
-    });
+app.controller('ShowAllMessages', function($scope, Dhis) {
+    $scope.dhis = new Dhis();
+    $scope.dhis.nextPage();
+});
+
+// Dhis constructor function to encapsulate HTTP and pagination logic
+app.factory('Dhis', function($http) {
+
+    $http.defaults.headers.common.Authorization = make_base_auth("admin", "district");
+
+    var Dhis = function() {
+        this.messages = [];
+        this.busy = false;
+        this.after = '';
+        this.page = 1;
+    };
+
+    Dhis.prototype.nextPage = function() {
+        if (this.busy) return;
+        console.log("loading page #" + this.page);
+        this.busy = true;
+
+        var url = dhisAPI + "/api/messageConversations.json?paging=true&pageSize=10&page="+this.page+"&fields=id,created,displayName,followUp,lastSenderSurname,lastSenderFirstname,read,messageCount";
+        $http.get(url).success(function(data) {
+            if(this.page <= data.pager.pageCount) {
+                this.messages = this.messages.concat(data.messageConversations);
+                this.page++;
+            }
+            this.busy = false;
+        }.bind(this));
+    };
+
+    return Dhis;
 });
 
 app.controller('ShowMessage', function($scope, $routeParams) {
@@ -58,3 +78,4 @@ app.config(function($routeProvider, $locationProvider) {
     // configure html5 to get links working on jsfiddle
     // $locationProvider.html5Mode(true);
 });
+
