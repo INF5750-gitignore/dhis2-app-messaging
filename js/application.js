dd894c5c78bc00923cb3206116e92043d9da3074
Øystein Dale <oystedal@ifi.uio.no>
2014-11-26 17:46:08 +0100

    Fixed bulk read/unread updates. Added vertical spacing.

--- js/application.js
+++ js/application.js
@@ -16,20 +16,23 @@ app.controller('ShowAllMessages', function($scope, $cachedResource, $filter, $ht
         // TODO: Add some limit
         for(var i = 0; i < msgs.length; i++) {
             MessageDetails.get({id: msgs[i].id});
-
         }
 
+        if (isDev) {
+            console.log("localStorage usage:", JSON.stringify(localStorage).length);
+        }
     });
 
     //filter the displayed messages
-    $scope.filters= [
-    {name:"all", id:0, display:"All Messages"}, 
+    $scope.filters = [
+    {name:"all", id:0, display:"All Messages"},
     {name:"follow", id:1, display:"Follow-Up"},
     {name:"unread", id:2, display:"Unread"}];
 
     $scope.messageFilter=$scope.filters[0];
-    
 
+    // Inconsistent API, names are stored differently when fetching a list
+    // compared to fetching a specific message.
     $scope.getName = function(msg) {
         if (msg.lastSender) {
             return msg.lastSender.name;
@@ -48,45 +51,48 @@ app.controller('ShowAllMessages', function($scope, $cachedResource, $filter, $ht
         return false;
     }
 
+    $scope.getVisibleMessages = function() {
+        // TODO: Replace the complex filtering in the view with this function
+    }
+
     $scope.unreadMessages = function(msg) {
-        if(msg.read==false)
-        {
-            return true;
+        return !msg.read;
+    }
+
+    $scope.updateReadStatus = function(setRead) {
+        var selected = [];
+
+        for (var i = 0; i < $scope.messages.length; i++) {
+            var message = $scope.messages[i];
+            if (message._selected && message.read != setRead) {
+                selected.push(message);
+            }
         }
-        return false;
 
-    }
+        if (selected.length === 0) return;
 
-    $scope.markReadID = function(id) {
-
-        console.log("plop");
+        var ids = selected.map(function(msg) { return msg.id; });
 
         $http({
             method: 'POST',
-            url: dhisAPI + 'api/messageConversations/read',
+            url: dhisAPI + 'api/messageConversations/' + (setRead ? 'read' : 'unread'),
             transformRequest: function(data) {
-                return "[\""+id+"\"]";
+                return JSON.stringify(ids);
             },
-            transformResponse: [],
+            transformResponse: function(data) {
+                return (setRead ? angular.fromJson(data).markedRead : angular.fromJson(data).markedUnread);
+            }
+        }).success(function(data) {
+            // TODO: This can be made more efficient with a map structure
+            for (var i = 0; i < data.length; i++) {
+                for (var j = 0; j < selected.length; j++) {
+                    if (data[i] === selected[j].id) {
+                        selected[j].read = setRead;
+                        break;
+                    }
+                }
+            }
         });
-    }
-
-    $scope.markUnreadID = function(id) {
-        $http({
-            method: 'POST',
-            url: dhisAPI + 'api/messageConversations/unread',
-            transformRequest: function(data) {
-                return "[\""+id+"\"]";
-            },
-            transformResponse: [],
-
-        });
-    }
-
-
-    $scope.testFunction = function(){
-
-        console.log("test function");
     }
 });
 
@@ -94,7 +100,7 @@ app.controller('ShowAllMessages', function($scope, $cachedResource, $filter, $ht
 var msgFields="id,displayName,read,lastSender,lastSenderFirstname,lastSenderSurname,followUp,created,messageCount";
 
 app.factory('Message', function($cachedResource, $http) {
-    return $cachedResource('message', "/api/messageConversations/:id.json?paging=false", {id: "@id"}, {
+    var Message = $cachedResource('message', "/api/messageConversations/:id.json?paging=false", {id: "@id"}, {
         'get': {
             method: 'GET',
             params: {
@@ -115,7 +121,22 @@ app.factory('Message', function($cachedResource, $http) {
                 return angular.fromJson(data).messageConversations;
             }
         },
+        'page': {
+            method: 'GET',
+            params: {
+                page: "@page",
+            },
+            url: dhisAPI + 'api/messageConversations.json?paging=true&pageSize=10&page=:page&fields=' + msgFields,
+            isArray: true,
+            transformResponse: function(data) {
+                return angular.fromJson(data).messageConversations;
+            }
+        }
     });
+
+    Message.prototype._selected = false;
+
+    return Message;
 });
 
 var markRead = function(msg, $http) {
@@ -140,15 +161,14 @@ var markUnread = function(msg, $http) {
     });
 }
 
+//var msgDetailFields = "created,followUp,name,read,messageCount,displayName,lastSender,messages"
+var msgDetailFields = "*";
+
 app.factory('MessageDetails', function($cachedResource) {
-    return $cachedResource('messageDetails', dhisAPI + '/api/messageConversations/:id/messages.json?fields=*', {id: "@id"});
+    return $cachedResource('messageDetails', dhisAPI + '/api/messageConversations/:id/messages.json?fields=' + msgDetailFields, {id: "@id"});
 });
 
 app.controller('ShowMessage', function($scope, $http, $routeParams, $cachedResource, $location, Message, MessageDetails) {
-    if (isDev) {
-        $http.defaults.headers.common.Authorization = make_base_auth("admin", "district");
-    }
-
     var msg = Message.get({id:$routeParams.msgId});
 
     msg.$httpPromise.then(function() {
@@ -179,18 +199,22 @@ app.controller('ShowMessage', function($scope, $http, $routeParams, $cachedResou
     $scope.conversationDetails = details;
 });
 
-app.run(function($window, $rootScope) {
-  $rootScope.online = navigator.onLine;
-  $window.addEventListener("offline", function () {
-    $rootScope.$apply(function() {
-      $rootScope.online = false;
-  });
-}, false);
-  $window.addEventListener("online", function () {
-    $rootScope.$apply(function() {
-      $rootScope.online = true;
-  });
-}, false);
+app.run(function($window, $http, $rootScope) {
+    if (isDev) {
+        $http.defaults.headers.common.Authorization = make_base_auth("admin", "district");
+    }
+
+    $rootScope.online = navigator.onLine;
+    $window.addEventListener("offline", function () {
+        $rootScope.$apply(function() {
+            $rootScope.online = false;
+        });
+    }, false);
+    $window.addEventListener("online", function () {
+        $rootScope.$apply(function() {
+            $rootScope.online = true;
+        });
+    }, false);
 });
 
 
