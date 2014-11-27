e371fc126f4520beed8da64ee4d2b155f9d20e1a
Øystein Dale <oystedal@ifi.uio.no>
2014-11-27 21:38:27 +0100

    Checking response code instead of online status
    So that it actually works during offline -> online transition

--- js/application.js
+++ js/application.js
@@ -225,8 +225,8 @@ app.controller('ShowMessage', function($scope, $http, $routeParams, $cachedResou
 
     msg.$httpPromise.then(function() {
         $scope.conversation = msg;
-    }, function() {
-        if ($rootScope.online) $location.path("/all");
+    }, function(response) {
+        if (response.status === 404) $location.path("/all");
     });
 
     var details = MessageDetails.get({id:$routeParams.msgId});
