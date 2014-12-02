9198bdef8e5a4a911c737e255cc40de623ee5767
Øystein Dale <oystedal@ifi.uio.no>
2014-12-02 22:26:36 +0100

    Added user resource

--- js/application.js
+++ js/application.js
@@ -220,6 +220,25 @@ app.factory('Message', function($cachedResource, $http) {
     return Message;
 });
 
+app.factory('User', function($cachedResource) {
+    var User = $cachedResource('user', "api/users.json", {id: "@id"}, {
+        'all': {
+            method: 'GET',
+            params: {
+                fields: "id,name",
+                paging: false,
+            },
+            url: dhisAPI + 'api/users.json',
+            isArray: true,
+            transformResponse: function(data) {
+                return angular.fromJson(data).users;
+            }
+        },
+    });
+
+    return User;
+});
+
 var markRead = function(msg, $http) {
     $http({
         method: 'POST',
